2816e1eb66a359c9bd31162c81eb19b153ce8eca
unknown <kibisama9@gmail.com>
2026-09-07 18:26:25 -0700

    temp

--- models/deliveryLog.js
+++ models/deliveryLog.js
@@ -11,6 +11,7 @@ const deliveryLogSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: "Delivery Station",
     required: true,
+    index: true,
   },
   session: {
     type: String,
